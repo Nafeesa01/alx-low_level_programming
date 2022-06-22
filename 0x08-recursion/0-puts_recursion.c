1428650b4f753c7a8d04f16ae9d7c0df13cf5274
Nafisa <nafisaissa2001@gmail.com>
2022-06-23 00:53:41 +0300

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion - a function to print string
+ * _puts_recursion - a function to print string recursively
  * @s: string parameter
  */
 void _puts_recursion(char *s)
